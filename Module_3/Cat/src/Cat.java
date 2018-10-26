e92c6875291062a2b0fe90153b84c05eac2cee9d
Emessar <arccassin@gmail.com>
2018-10-26 22:57:26 +0300

    new

---
+++ Module_3/Cat/src/Cat.java
@@ -0,0 +1,103 @@
+
+public class Cat
+{
+    private Double originWeight;
+    private Double weight;
+
+    private Double feedWeigth = 0.0;
+    private Double minWeight;
+    private Double maxWeight;
+
+    private boolean liveStatus = true;
+
+    private static Integer count = 0;
+
+    public static Integer getCount()
+    {
+        return count;
+    }
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+        Cat.count = Cat.count + 1;
+    }
+
+    public Cat(Double catWeight)
+    {
+        weight = catWeight;
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+        count = Cat.count + 1;
+
+    }
+
+    public void meow()
+    {
+        weight = weight - 1;
+        System.out.println("Meow");
+        if (weight < minWeight && liveStatus)
+        {
+            Cat.count = Cat.count - 1;
+            liveStatus = false;
+        }
+    }
+
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+        feedWeigth = feedWeigth + amount;
+        if (weight > maxWeight && liveStatus)
+        {
+            Cat.count = Cat.count - 1;
+            liveStatus = false;
+        }
+    }
+
+    public void drink(Double amount)
+    {
+        weight = weight + amount;
+        if (weight > maxWeight && liveStatus)
+        {
+            Cat.count = Cat.count - 1;
+            liveStatus = false;
+        }
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight) {
+            return "Dead";
+        }
+        else if(weight > maxWeight) {
+            return "Exploded";
+        }
+        else if(weight > originWeight) {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+
+    public Double getEatenMass()
+    {
+        return feedWeigth;
+
+    }
+
+    public void goToilet()
+    {
+        weight = weight - 10;
+        System.out.println("Terrorist win!");
+    }
+}
---
+++ Module_3/Cat/src/Loader.java
@@ -0,0 +1,43 @@
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        Cat nuasha = new Cat();
+        Cat tisha = new Cat();
+        Cat sima = new Cat();
+        Cat yarsik = new Cat();
+        Cat asya = new Cat();
+        Cat begemot = new Cat();
+
+        nuasha.getWeight();
+        for (int i = 0; i < 3500; i++)
+        {
+            nuasha.feed(2.0);
+            tisha.meow();
+            sima.drink(1.0);
+        }
+
+        yarsik.feed(350.0);
+        asya.drink(100.0);
+
+        System.out.println(nuasha.getStatus());
+        System.out.println(tisha.getStatus());
+        System.out.println(sima.getStatus());
+        System.out.println(yarsik.getStatus());
+        System.out.println(asya.getStatus());
+        System.out.println(begemot.getStatus());
+
+        System.out.println("Cat Count: " + Cat.getCount());
+
+        Cat kitCat = getKitten();
+        System.out.println("kitCat weight: " + kitCat.getWeight());
+    }
+
+    public static Cat getKitten()
+    {
+        Double catWeight = 100. + 100. * Math.random();
+        Cat cat = new Cat(catWeight);
+        return cat;
+    }
+}
