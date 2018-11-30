a55c893cc7e616938efd2ff1416e9510478b3ab8
Emessar_WinPC <arccassin@gmail.com>
2018-11-30 22:19:39 +0300

    edt

--- Module_4/StringExperiments/src/Loader.java
+++ Module_4/StringExperiments/src/Loader.java
@@ -6,9 +6,22 @@ public class Loader
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 
         //Вася и Миша
-        int beginIndex = text.indexOf("л") + 1;
-        System.out.println("Вася: " + text.substring(beginIndex, beginIndex + 5));
-        beginIndex = text.lastIndexOf("-") + 1;
-        System.out.println("Миша: " + text.substring(beginIndex, beginIndex + 6));
+        int beginIndex = text.indexOf(" ") + 1;
+        String text2 = text.substring(beginIndex);
+        beginIndex = text2.indexOf(" ") + 1;
+        String text3 = text2.substring(beginIndex);
+        beginIndex = text3.indexOf(" ");
+
+        int firstNum = Integer.parseInt(text3.substring(0, beginIndex));
+
+        beginIndex = text3.lastIndexOf("-") + 2;
+        String text4 = text3.substring(beginIndex);
+        beginIndex = text4.indexOf(" ");
+
+        int secondNum = Integer.parseInt(text4.substring(0, beginIndex));
+
+        System.out.println("Вася: " + firstNum);
+
+        System.out.println("Миша: " + secondNum);
     }
 }
