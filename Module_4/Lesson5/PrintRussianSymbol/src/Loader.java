81290698696aca6163794acf3d51e8081b38889c
Emessar_WinPC <arccassin@gmail.com>
2018-12-04 23:05:54 +0300

    add

---
+++ Module_4/Lesson5/PrintRussianSymbol/src/Loader.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Emessar_WinPC on 04 Дек., 2018
+ */
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1040; i < 1072; i++)
+        {
+            System.out.println((char)i + ": " + i + "           " + (char)(i + 32) + ": " + (i + 32));
+        }
+
+    }
+}
