26e96dca2ec6eff925f99ac5752158b3f2e6764b
Emessar_WinPC <arccassin@gmail.com>
2018-12-06 22:00:52 +0300

    edt

--- Module_4/Lesson4/StringExperimentsNotRegular/src/Loader.java
+++ Module_4/Lesson4/StringExperimentsNotRegular/src/Loader.java
@@ -3,7 +3,7 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        String text = "Вася заработал 50001 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 
         //Вася и Миша
         int beginIndex = text.indexOf(" ") + 1;
@@ -20,8 +20,6 @@ public class Loader
 
         int secondNum = Integer.parseInt(text.substring(0, beginIndex));
 
-        System.out.println("Вася: " + firstNum);
-
-        System.out.println("Миша: " + secondNum);
+        System.out.println("Сумма: " +  (firstNum + secondNum));
     }
 }
