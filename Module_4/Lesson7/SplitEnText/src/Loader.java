3053c8508db166a5b9a7fdfe1fa20709e92000c5
arccassin <arccassin@gmail.com>
2019-06-05 23:01:51 +0300

    done

--- Module_4/Lesson7/SplitEnText/src/Loader.java
+++ Module_4/Lesson7/SplitEnText/src/Loader.java
@@ -11,9 +11,14 @@ public class Loader
                 "the weak through the valley of darkness, for he is truly his brother’s keeper and the finder " +
                 "of lost children. And I will strike down upon thee with great vengeance and furious anger those" +
                 " who would attempt to poison and destroy my brothers. And you will know my name is the Lord when " +
-                "I lay my vengeance upon thee.”\n" +
-                "\n";
+                "I lay my vengeance upon thee.”";
 
+        text = text.replaceAll("\\W", " ");
 
+        String[] strings = text.split("\\s+");
+        for (int i = 0; i < strings.length; i++)
+        {
+            System.out.println(strings[i]);
+        }
     }
 }
