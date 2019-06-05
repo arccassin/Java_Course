cde6fcd7a93a836423c82bd5558c56f4ed36b579
arccassin <arccassin@gmail.com>
2019-06-05 22:38:19 +0300

    edit regexp

--- Module_4/Lesson7/StringExperimentsRegular/src/Loader.java
+++ Module_4/Lesson7/StringExperimentsRegular/src/Loader.java
@@ -3,11 +3,13 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей. А Олегу папа дал 100000";
 
-        text = text.replaceAll("[^0-9,//s]", "");
+        text = text.replaceAll("[^0-9\\s]", "");
 
-        String summStr[] = text.split(",");
+        text = text.trim();
+
+        String summStr[] = text.split("\\s+");
 
         int summ = 0;
         for (int i = 0; i < summStr.length; i++)
