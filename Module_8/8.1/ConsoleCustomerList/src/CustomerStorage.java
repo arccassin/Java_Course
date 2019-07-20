dfb83d81294ad7ee53b82d4663b36775e4a5fefe
arccassin <arccassin@gmail.com>
2019-07-20 22:14:21 +0300

    Add

--- Module_8/8.1/ConsoleCustomerList/src/CustomerStorage.java
+++ Module_8/8.1/ConsoleCustomerList/src/CustomerStorage.java
@@ -1,33 +1,44 @@
 import java.util.HashMap;
 
-public class CustomerStorage
-{
+public class CustomerStorage {
+    private static String addCommand = "add Василий Петров " +
+            "vasily.petrov@gmail.com +79215637722";
     private HashMap<String, Customer> storage;
 
-    public CustomerStorage()
-    {
+    public CustomerStorage() {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data)
-    {
+    public void addCustomer(String data) {
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Error adding customer: wrong format. " +
+                    "Correct format:\n" + addCommand);
+        }
         String name = components[0] + " " + components[1];
-        storage.put(name, new Customer(name, components[3], components[2]));
+
+        String phone = components[3];
+        if (!((phone.length() == 12 && phone.indexOf("+7") == 0 || (phone.length() == 11 && phone.charAt(0) == '8')))) {
+            throw new IllegalArgumentException("Phone \"" + phone + "\" not correct!");
+        }
+
+        String email = components[2];
+        if (!(email.matches("^.+@.+"))) {
+            throw new IllegalArgumentException("E-mail \"" + email + "\" not correct!");
+        }
+
+        storage.put(name, new Customer(name, phone, email));
     }
 
-    public void listCustomers()
-    {
+    public void listCustomers() {
         storage.values().forEach(System.out::println);
     }
 
-    public void removeCustomer(String name)
-    {
+    public void removeCustomer(String name) {
         storage.remove(name);
     }
 
-    public int getCount()
-    {
+    public int getCount() {
         return storage.size();
     }
 }
